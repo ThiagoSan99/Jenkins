1460e8fb760b233f9d296a9f900f8007e1844fba
David Enriquez <iemccp7.5enriquezdavidmail.com>
2023-11-13 17:47:07 -0500

    The commit has been made

---
+++ Ej1Jenkins/src/ej1jenkins/Ej1Jenkins.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej1jenkins;
+
+/**
+ *
+ * @author Hewlett Packard
+ */
+public class Ej1Jenkins {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" The commit has been made ");
+    }
+    
+}
